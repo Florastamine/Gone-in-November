66a98f6010db9f44cae477c1bc544850ec21de16
Florastamine <remilla.scarlet.94@gmail.com>
2015-11-26 10:38:46 +0700

    Work finished on <common>: PhysX, logging, game state.

--- common.c
+++ common.c
@@ -13,21 +13,228 @@
  *    TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
  * 
  *   0. You just DO WHAT THE FUCK YOU WANT TO.
+ * __________________________________________________________________
+ * 
+ * <common>
+ * Place for common game code related to the game logic resides. 
+ * Responsible for general-purpose game functionalities (scene loading, asset loading, ...)
+ * and managing (loading/unloading) subsystems (physics, audio, logging,...).
+ * 
+ * Authors: Huy Nguyen (http://vn-sharing.net/forum/member.php?u=15466)
+ * __________________________________________________________________
  */
-void game_physx_new()
+#define __LOVELY_21_HYPHENS "---------------------" // It's legit
+
+/*
+ * void game_state_new()
+ * 
+ * Allocates a new game state, and sets up the events for freeing the
+ * state on game exit. This function must be called before 
+ * any other subsystems can be initialized.
+ */
+void game_state_new()
 {
-	if( !__game_physx_loaded__ )
+	// Free the previously allocated game state, if it was allocated before.
+	game_state_free();
+	
+	// Now initialize the state.
+	__GameState_singleton = MALLOC(1, GameState);
+	
+	__GameState_singleton->__game_physx_loaded__ = false;
+	__GameState_singleton->__game_log_loaded__   = false;
+	__GameState_singleton->__game_log_handle__   = 0;
+	
+	// And call the event setup (so that the state and some other stuff gets automatically destroyed prior to exiting the game.)
+	game_event_setup();
+}
+
+/*
+ * void game_state_free()
+ * 
+ * Frees a previously allocated game state.
+ */
+void game_state_free()
+{
+	if( __GameState_singleton )
 	{
-		physX_open();
-		__game_physx_loaded__ = true;
+		// Because the game state holds the "switch" variables for every subsystems, 
+		// we have to free the subsystems first prior to freeing the game state itself.
+		// Thus, calling game_state_free() effectively frees the subsystems.
+		game_physx_free();
+		game_log_free();
+		
+		// Now free the game state.
+		FREE(__GameState_singleton);
 	}
 }
 
+/*
+ * void game_physx_new()
+ * 
+ * Initializes PhysX.
+ */
+void game_physx_new()
+{
+	if( !__GameState_singleton->__game_physx_loaded__ )
+	{	
+		// Initializes the PhysX subsystem. Straight outta <ackPhysX>
+		if( !NxPhysicsSDK )
+		{
+			NxPhysicsSDK = physX_load();
+			pX_setsteprate(60.0, 8.0, NX_TIMESTEP_FIXED);
+			NxScene = physX_run(0);
+			pX_setunit(0.05);
+			pX_setccd(1);
+			
+			#ifdef    _UTILITIES_H_ /* It's merely the same. */
+		        pX_setgravity(vector(0, 0, -GRAVITY_ACCELERATION));
+		   #else
+		        pX_setgravity(vector(0, 0, -9.81));
+		   #endif
+		   
+		   ph_fps_max_lock = 60.0;
+		   ph_check_distance = 2;
+		   
+		   // Register additional events (can't be put into game_event_setup() - maybe we don't need unnecessary functions
+		   // and (possible) crashs). ;)
+		   if( !on_exit )         on_exit = on_exit_px0;
+		   if( !on_level_load )   on_level_load = on_level_load_px0;
+		   if( !on_ent_remove )   on_ent_remove = on_ent_remove_px0;
+		   
+		   on_exit_px = on_exit;	// store previous on_exit function
+		   on_exit = physX_close;
+		   on_level_load_px = on_level_load;
+		   on_level_load = physX_level_load;
+		   on_ent_remove_px = on_ent_remove;
+		   on_ent_remove = physX_ent_remove;
+		   
+		   __GameState_singleton->__game_physx_loaded__ = true;
+		}
+	}
+}
+
+/*
+ * void game_physx_loop()
+ *
+ * Runs the PhysX simulation. For continuous physics simulation, you 
+ * must place this function in a main loop whenever you have initialized PhysX or not.
+ * Code taken from <ackPhysX> with a small modification; the algorithm provided in the 
+ * manual is wrong and outdated.
+ */
+void game_physx_loop()
+{
+	if(NxPhysicsSDK)
+	{
+		#ifdef FIXED_TIME //This for-loop fixes the physX framerate to 60 when the frame rate is below 60 fps
+			int loc_n = integer(time_step * 60 / 16) + 1, loc_i = 0;
+			
+			if (freeze_mode <= 0)
+			    for (; loc_i < loc_n; loc_i++)
+			        physX_run(0);
+			#else
+			if (freeze_mode <= 0)		
+			    physX_run(time_frame/16);
+		#endif
+		proc_mode = PROC_EARLY;	// call physX_run() before entity actions.
+	}
+}
+
+/*
+ * void game_physx_free()
+ * 
+ * Frees PhysX.
+ */
 void game_physx_free()
 {
-	if( __game_physx_loaded__ )
+	if( __GameState_singleton->__game_physx_loaded__ )
 	{
 		physX_destroy();
-		__game_physx_loaded__ = false;
+		__GameState_singleton->__game_physx_loaded__ = false;
 	}
+}
+
+/*
+ * void game_log_new()
+ * 
+ * Opens the logging file.
+ */
+void game_log_new()
+{
+	if( !__GameState_singleton->__game_log_loaded__ && !__GameState_singleton->__game_log_handle__ )
+	{
+		__GameState_singleton->__game_log_handle__ = file_open_write(__LOG_FILE);
+		__GameState_singleton->__game_log_loaded__ = (BOOL) ifelse(__GameState_singleton->__game_log_handle__, true, false);
+		
+		if( __GameState_singleton->__game_log_loaded__ )
+		    game_log_write(__LOVELY_21_HYPHENS); // It's legit
+	}
+}
+
+/*
+ * void game_log_free()
+ * 
+ * Frees the logging file. Note that, because the file is opened in write mode 
+ * (not append!), any other calls to game_log_new() after game_log_free() 
+ * will cause the old log file to be erased.
+ * 
+ * TODO: Append instead of a complete erase.
+ */
+void game_log_free()
+{
+	if( __GameState_singleton->__game_log_loaded__ )
+	{
+		game_log_write(__LOVELY_21_HYPHENS); // It's legit
+		
+		file_close(__GameState_singleton->__game_log_handle__);
+		__GameState_singleton->__game_log_loaded__ = false;
+	}
+}
+
+/*
+ * void game_log_write(const STRING *content)
+ * 
+ * Writes a string to the log file.
+ * Information about time and date are automatically appended.
+ */
+void game_log_write(const STRING *content)
+{
+	if( __GameState_singleton->__game_log_loaded__ )
+	{
+		// Straight outta <utilities>
+		char *cstr = MALLOC(256, char);
+		sprintf( cstr, "[%i:%i, %i.%i.%i] %s",
+		(int) sys_hours,
+		(int) sys_minutes,
+		(int) sys_day,
+		(int) sys_month,
+		(int) sys_year,
+		ifelse(content, _chr(content), _chr("Undescribed log message.")) );
+		
+		file_str_write(__GameState_singleton->__game_log_handle__, cstr);
+		file_asc_write(__GameState_singleton->__game_log_handle__, 13);
+		file_asc_write(__GameState_singleton->__game_log_handle__, 10);
+		
+		FREE(cstr);
+	}
+}
+
+__static void __game_event_on_close()
+{
+	game_state_free();
+}
+
+__static void __game_event_on_exit()
+{
+	game_state_free();
+}
+
+/*
+ * void game_event_setup()
+ * 
+ * Sets up certain events that are automatically invoked later.
+ */
+void game_event_setup()
+{
+	on_close         = __game_event_on_close;
+	on_exit          =  __game_event_on_exit;
 }
--- common.h
+++ common.h
@@ -13,6 +13,15 @@
  *    TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
  * 
  *   0. You just DO WHAT THE FUCK YOU WANT TO.
+ * __________________________________________________________________
+ * 
+ * <common>
+ * Place for common game code related to the game logic resides. 
+ * Responsible for general-purpose game functionalities (scene loading, asset loading, ...)
+ * and managing (loading/unloading) subsystems (physics, audio, logging,...).
+ * 
+ * Authors: Huy Nguyen (http://vn-sharing.net/forum/member.php?u=15466)
+ * __________________________________________________________________
  */
 #ifndef    _COMMON_H_
 #define    _COMMON_H_
@@ -29,10 +38,29 @@
     #define    PRAGMA_ZERO
 #endif
 
-BOOL __game_physx_loaded__ = false;
+#define __LOG_FILE "stdout.log"
+
+typedef struct {
+	BOOL   __game_physx_loaded__;
+	
+	BOOL   __game_log_loaded__;
+	fixed  __game_log_handle__;
+} GameState;
+
+GameState *__GameState_singleton = NULL;
+
+void game_state_new();
+void game_state_free();
 
 void game_physx_new();
+void game_physx_loop();
 void game_physx_free();
+
+void game_log_new();
+void game_log_free();
+void game_log_write(const STRING *content);
+
+void game_event_setup();
 
 #include "common.c"
 #endif /* common.h */
