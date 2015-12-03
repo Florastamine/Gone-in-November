ef7f74e945f8dcdce1a9535f54bb7f00cc7d4020
Florastamine <remilla.scarlet.94@gmail.com>
2015-12-03 13:25:07 +0700

    Very simple game_scene_load()

--- common.c
+++ common.c
@@ -285,3 +285,324 @@ void game_event_setup()
 	on_close         = __game_event_on_close;
 	on_exit          = __game_event_on_exit;
 }
+
+__static void __p_set_translucent( PANEL *p )
+{
+	if(p)
+	    if( !(p->flags & TRANSLUCENT) )
+	        p->flags |= (TRANSLUCENT);
+}
+
+__static void __p_fade_in( PANEL *p, float f, float t, float s )
+{
+	if(p) {
+		if( f > t ) swap( &f, &t );
+		__p_set_translucent(p);
+		
+		p->alpha = f;
+		while(p->alpha < t) {
+			p->alpha += s * time_step;
+			
+			wait(1.0);
+		}
+	}
+}
+
+__static void __p_fade_out( PANEL *p, float f, float t, float s )
+{
+	if(p) {
+		if( t > f ) swap( &f, &t );
+		__p_set_translucent(p);
+		
+		p->alpha = f;
+		while(p->alpha > t) {
+			p->alpha -= s * time_step;
+			
+			wait(1.0);
+		}
+	}
+}
+
+__static void __game_scene_set_defaults()
+{
+	pair_set( &__SceneLoadState_singleton->load_text_pos, screen_size.x / 2, screen_size.y - 150.0 );
+	pair_set( &__SceneLoadState_singleton->desc_text_pos, screen_size.x / 2, screen_size.y - 250.0 );
+	pair_set( &__SceneLoadState_singleton->load_img_pos, 0.0, 0.0 );
+	
+	__SceneLoadState_singleton->load_img = bmap_createblack(screen_size.x, screen_size.y, 8); // 8-bit black bitmap
+	__SceneLoadState_singleton->load_text = str_create("Loading...");
+	__SceneLoadState_singleton->desc_text = str_create("The quick brown fox jumps over the lazy dog...");
+	
+	__SceneLoadState_singleton->error      = 0;
+	__SceneLoadState_singleton->delay      = 0.0;
+	__SceneLoadState_singleton->fade       = false;
+	__SceneLoadState_singleton->fade_speed = 10.0;
+	
+	__SceneLoadState_singleton->load_text_font = font_create("Arial#15b");
+	__SceneLoadState_singleton->desc_text_font = font_create("Arial#15b");
+}
+
+// Users don't need to care about allocating __SceneLoadState_singleton 
+__static void __game_scene_state_new()
+{
+	if( !__SceneLoadState_singleton )
+	{
+		__SceneLoadState_singleton             = MALLOC(1, SceneLoadState);
+		
+		__game_scene_set_defaults();
+	}
+}
+
+__static void __game_scene_state_free()
+{
+	if( __SceneLoadState_singleton )
+	{
+		bmap_remove(__SceneLoadState_singleton->load_img);
+		str_remove(__SceneLoadState_singleton->load_text);
+		str_remove(__SceneLoadState_singleton->desc_text);
+		font_remove(__SceneLoadState_singleton->load_text_font);
+		font_remove(__SceneLoadState_singleton->desc_text_font);
+		
+		FREE(__SceneLoadState_singleton);
+	}
+}
+
+void game_scene_set_defaults()
+{
+	__game_scene_set_defaults();
+}
+
+void game_scene_set_load_text_pos(float x, float y)
+{
+	__game_scene_state_new();
+	
+	pair_set( &__SceneLoadState_singleton->load_text_pos, x, y );
+}
+
+void game_scene_set_desc_text_pos(float x, float y)
+{
+	__game_scene_state_new();
+	
+	pair_set( &__SceneLoadState_singleton->desc_text_pos, x, y );
+}
+
+void game_scene_set_load_img_pos(float x, float y)
+{
+	__game_scene_state_new();
+	
+	pair_set( &__SceneLoadState_singleton->load_img_pos, x, y );
+}
+
+void game_scene_set_load_screen( __In STRING *load_img )
+{
+	__game_scene_state_new();
+	
+	if(file_exists(load_img))
+	    __SceneLoadState_singleton->load_img = bmap_create(load_img);
+}
+
+BMAP *game_scene_get_load_screen()
+{
+	if(__SceneLoadState_singleton)
+	{
+		if(__SceneLoadState_singleton->load_img)
+		    return __SceneLoadState_singleton->load_img;
+	}
+	
+	return 0;
+}
+
+void game_scene_set_load_text( __In STRING *load_text )
+{
+	__game_scene_state_new();
+	
+	if(load_text)
+	    str_cpy( __SceneLoadState_singleton->load_text, load_text );
+}
+
+STRING *game_scene_get_load_text()
+{
+	if(__SceneLoadState_singleton)
+	{
+		if(__SceneLoadState_singleton->load_text)
+		    return __SceneLoadState_singleton->load_text;
+	}
+	
+	return str_create("NULL"); // ;). A string containing "NULL" is far more surprise than just a NULL-ed string.
+}
+
+void game_scene_set_desc_text( __In STRING *desc_text )
+{
+	__game_scene_state_new();
+
+	if(desc_text)
+	    str_cpy( __SceneLoadState_singleton->desc_text, desc_text );
+}
+
+STRING *game_scene_get_desc_text()
+{
+	if(__SceneLoadState_singleton)
+	{
+		if(__SceneLoadState_singleton->desc_text)
+		    return __SceneLoadState_singleton->desc_text;
+	}
+	
+	return str_create("NULL");
+}
+
+void game_scene_set_load_text_font( __In FONT *font_struct )
+{
+	__game_scene_state_new();
+	
+	__SceneLoadState_singleton->load_text_font = font_struct;
+}
+
+void game_scene_set_load_text_font( __In STRING *font_str )
+{
+	game_scene_set_load_text_font(font_create(font_str));
+}
+
+FONT *game_scene_get_load_text_font()
+{
+	if(__SceneLoadState_singleton)
+	{
+		if(__SceneLoadState_singleton->load_text_font)
+		    return __SceneLoadState_singleton->load_text_font;
+	}
+	
+	return NULL;
+}
+
+void game_scene_set_desc_text_font( __In FONT *font_struct )
+{
+	__game_scene_state_new();
+	
+	__SceneLoadState_singleton->load_text_font = font_struct;
+}
+
+void game_scene_set_desc_text_font( __In STRING *font_str )
+{
+	game_scene_set_desc_text_font(font_create(font_str));
+}
+
+FONT *game_scene_get_desc_text_font()
+{
+	if(__SceneLoadState_singleton)
+	{
+		if(__SceneLoadState_singleton->load_text_font)
+		    return __SceneLoadState_singleton->load_text_font;
+	}
+	
+	return NULL;
+}
+
+void game_scene_set_delay( __In float d )
+{
+	if(__SceneLoadState_singleton)
+	    __SceneLoadState_singleton->delay = (float) abs(d);
+}
+
+float game_scene_get_delay()
+{
+	if(__SceneLoadState_singleton)
+	    return __SceneLoadState_singleton->delay;
+}
+
+void game_scene_set_fade( __In BOOL b )
+{
+	if(__SceneLoadState_singleton)
+	    __SceneLoadState_singleton->fade = b;
+}
+
+void game_scene_set_fade_speed( __In float s )
+{
+	if(__SceneLoadState_singleton)
+	    __SceneLoadState_singleton->fade_speed = s;
+}
+
+float game_scene_get_fade_speed()
+{
+	if(__SceneLoadState_singleton)
+	    return __SceneLoadState_singleton->fade_speed;
+}
+
+BOOL  game_scene_is_fade()
+{
+	if(__SceneLoadState_singleton)
+	    return __SceneLoadState_singleton->fade;
+}
+
+/*
+ * void game_scene_load( STRING *scene )
+ * 
+ * A level_load() wrapper with some GUI elements like loading screen and text.
+ * First, set up the various GUI elements by calling the game_scene_set*() functions.
+ * This function mimics OpenGL style: Everything is stored in a state object, meaning 
+ * if the data set is kept unchanged, or if you only want to change a small portion of the state,
+ * this normally could be resolved to only one function call, rather than a large ass function
+ * with lots and lots of parameters.
+ * And you can even query the state using get() functions.
+ */
+void game_scene_load( STRING *scene )
+{
+	__game_scene_state_new();
+	
+	PANEL *p  = pan_create(NULL, 1);
+	TEXT  *t0 = txt_create(1, 2);
+	TEXT  *t1 = txt_create(1, 2);
+	
+	// Feed the structs' data with the ones fed from the singleton.
+	p->bmap = game_scene_get_load_screen();
+	p->pos_x = (__SceneLoadState_singleton->load_img_pos).first;
+	p->pos_y = (__SceneLoadState_singleton->load_img_pos).second;
+	p->scale_x = screen_size.x / bmap_width(p->bmap);
+	p->scale_y = screen_size.y / bmap_height(p->bmap);
+	
+	str_cpy((t0->pstring)[0], game_scene_get_load_text());
+	str_cpy((t1->pstring)[0], game_scene_get_desc_text());
+	
+	t0->pos_x = (__SceneLoadState_singleton->load_text_pos).first;
+	t0->pos_y = (__SceneLoadState_singleton->load_text_pos).second;
+	t0->font  = game_scene_get_load_text_font();
+	
+	t1->pos_x = (__SceneLoadState_singleton->desc_text_pos).first;
+	t1->pos_y = (__SceneLoadState_singleton->desc_text_pos).second;
+	t1->font  = game_scene_get_desc_text_font();
+	
+	p->flags  |= (SHOW);
+	t1->flags |= (SHOW);
+	t0->flags |= (SHOW);
+	
+	if(game_scene_is_fade())
+	{
+		__p_fade_in(p, 0.0, 100.0, game_scene_get_fade_speed());
+		while(proc_status(__p_fade_in)) wait(1.0); // Block the local static function (because of wait(1) could allow
+		                                           // level_load() to be executed at the same time due to concurrency).
+	}
+	
+	level_load(scene);
+	while(proc_status(level_load)) wait(1.0); // Temporary blocking the main thread
+	
+	wait(1); // So last_error can become valid
+	__SceneLoadState_singleton->error = last_error;
+	last_error = 0;
+	
+	if( game_scene_get_delay() > 0.0 )
+	wait( - __SceneLoadState_singleton->delay ); // Note the minus sign - we want to wait in seconds.
+	
+	t1->flags &= ~(SHOW);
+	t0->flags &= ~(SHOW);
+	
+	if(game_scene_is_fade())
+	{
+		__p_fade_out(p, 100.0, 0.0, game_scene_get_fade_speed());
+		while(proc_status(__p_fade_out)) wait(1.0);
+	}
+
+	p->flags  &= ~(SHOW);
+	
+	// De-allocate resources.
+	txt_remove_ex(t0);
+	txt_remove_ex(t1);
+	pan_remove(p);
+}
--- common.h
+++ common.h
@@ -40,6 +40,10 @@
 
 #define __LOG_FILE "stdout.log"
 
+/*
+ * GameState (struct)
+ * This struct contains everything related to the current game state.
+ */
 typedef struct {
 	BOOL   __game_physx_loaded__;
 	
@@ -48,6 +52,35 @@ typedef struct {
 } GameState;
 
 GameState *__GameState_singleton = NULL;
+
+/*
+ * SceneLoadState (struct)
+ * This struct contains information about the loading state.
+ * When loading a scene with game_scene_load(), the information
+ * in this structure is used, so there are functions to alter the singleton
+ * (so you can set custom loading bitmap or text pos., more to come).
+ */
+typedef struct {
+	Vector2 load_text_pos;
+	Vector2 desc_text_pos;
+	Vector2 load_img_pos;
+	
+	Bitmap *load_img;
+	
+	String *load_text;
+	String *desc_text;
+	
+	Font   *load_text_font;
+	Font   *desc_text_font;
+	
+	float delay;
+	float fade_speed;
+	
+	int error;
+	BOOL fade;
+} SceneLoadState;
+
+SceneLoadState *__SceneLoadState_singleton = NULL;
 
 void game_state_new();
 void game_state_free();
@@ -58,9 +91,46 @@ void game_physx_free();
 
 void game_log_new();
 void game_log_free();
-void game_log_write(const STRING *content);
+void game_log_write( __In const STRING *content );
 
 void game_event_setup();
+
+// It'd be better to split this big ass function into a few smaller ones but doing that would 
+// lengthen the functions' name so... bear with it. ;(
+// Edited: Nailed it.
+void game_scene_set_defaults();
+
+void game_scene_set_load_screen( __In STRING *load_img );
+BMAP *game_scene_get_load_screen();
+
+void game_scene_set_load_text( __In STRING *load_text );
+STRING *game_scene_get_load_text();
+
+void game_scene_set_desc_text( __In STRING *desc_text );
+STRING *game_scene_get_desc_text();
+
+void game_scene_set_load_text_font( __In STRING *font_str );
+void game_scene_set_load_text_font( __In FONT *font_struct );
+FONT *game_scene_get_load_text_font();
+
+void game_scene_set_desc_text_font( __In STRING *font_str );
+void game_scene_set_desc_text_font( __In FONT *font_struct );
+FONT *game_scene_get_desc_text_font();
+
+void game_scene_set_load_text_pos(__In float x, __In float y);
+void game_scene_set_desc_text_pos(__In float x, __In float y);
+void game_scene_set_load_img_pos(__In float x, __In float y);
+
+void game_scene_set_delay( __In float d );
+float game_scene_get_delay();
+
+void game_scene_set_fade( __In BOOL b );
+void game_scene_set_fade_speed( __In float s );
+
+float game_scene_get_fade_speed();
+BOOL  game_scene_is_fade();
+
+void game_scene_load( __In STRING *scene );
 
 #include "common.c"
 #endif /* common.h */
