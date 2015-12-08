424c383ea321b790e90b5fe2d9d473c81be9c1cb
Florastamine <remilla.scarlet.94@gmail.com>
2015-12-08 09:04:33 +0700

    Force the scene file to recognize outside assets :umbrella:

---
+++ November.h
@@ -0,0 +1,6 @@
+/* November.h */
+#ifndef    __NOVEMBER_RAIN_H__
+#define    __NOVEMBER_RAIN_H__
+
+#include "November.c"
+#endif /* November.h */
---
+++ scene/scene.c
@@ -0,0 +1,9 @@
+/*
+ * <scene>
+ * 
+ * This is a simple code fragment which (along with scene.wdl), forces the .wmp files to recognize the
+ * code and assets outside of this folder. Must be assigned with every scene file.
+ */
+#define PRAGMA_PATH "../"
+
+#include "../November.h"
