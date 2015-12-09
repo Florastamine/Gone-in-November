b991b72ee65ec13f2139de090454dc22584a47f3
Florastamine <remilla.scarlet.94@gmail.com>
2015-12-09 12:04:43 +0700

    Accidentally deleted some code

--- behavior_player.c
+++ behavior_player.c
@@ -1,3 +1,195 @@
+/* behavior_player.c */
+/*
+ *             DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *                     Version 2, December 2004
+ * 
+ *  Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+ * 
+ *  Everyone is permitted to copy and distribute verbatim or modified
+ *  copies of this license document, and changing it is allowed as long
+ *  as the name is changed.
+ *
+ *             DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *    TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ * 
+ *   0. You just DO WHAT THE FUCK YOU WANT TO.
+ * __________________________________________________________________
+ * 
+ * <behavior_player>
+ * 
+ * Player code, first person, with credits goes to 3Run.
+ * I modified, optimized and simplified his original code for PRAGMA_POINTER
+ * compatibility and better coding structures. As a result (and as the nature of 
+ * GiN), the code won't include ducking and jumping.
+ * 
+ * Authors: Huy Nguyen (http://vn-sharing.net/forum/member.php?u=15466)
+ * __________________________________________________________________
+ */
+__act_player_state *act_player_get_singleton()
+{
+	if(__act_player_state_singleton)
+	    return __act_player_state_singleton;
+}
+
+void act_player_free()
+{
+	game_log_write("Request to free the soul from all the burdens.");
+	
+	if( __act_player_state_singleton )
+	{
+		dtimer();
+		game_log_write("Now freeing the soul.");
+		
+		safe_remove(__act_player_state_singleton->__object);
+		safe_remove(__act_player_state_singleton->__object_stand);
+		
+		FREE(__act_player_state_singleton);
+		
+		double d = dtimer();
+		game_log_write( str_printf(NULL, "The soul was freed within a total of %f seconds.", d*(10^6)) );
+	}
+}
+
+void act_player_new()
+{
+	game_log_write("Request to allocate a home for the soul...");
+	
+	if( !__act_player_state_singleton )
+	{
+		game_log_write("Home not found. Allocating a new one...");
+		dtimer();
+		
+		__act_player_state_singleton = MALLOC(1, __act_player_state);
+		
+		// Don't need to do MALLOC() for entity pointers because ent_create() will take
+		// care of that instead.
+		vec_zero(__act_player_state_singleton->__bbox_lowest_pos);
+		vec_zero(__act_player_state_singleton->__platform_move_speed);
+		vec_zero(__act_player_state_singleton->__cam_pos);
+		vec_zero(__act_player_state_singleton->__cam_ang);
+		
+		__act_player_state_singleton->__move_type                 = MOVE_ON_FOOT;
+		__act_player_state_singleton->state                       = STATE_IDLE;
+		__act_player_state_singleton->health                      = 100;
+		__act_player_state_singleton->cam_height                  = 60;
+		__act_player_state_singleton->cam_lerp                    = 0;
+		__act_player_state_singleton->cam_smooth                  = 0.9;
+		__act_player_state_singleton->cam_smooth_offset           = 2;
+		__act_player_state_singleton->friction                    = 2.0;
+		__act_player_state_singleton->movement_friction           = 0.0;
+		__act_player_state_singleton->movement_friction_air       = 0.4;
+		__act_player_state_singleton->movement_friction_ground    = 0.8;
+		__act_player_state_singleton->move_speed                  = 42 + 42 + 21; /* ;) */
+		__act_player_state_singleton->climb_speed                 = 2.5;
+		__act_player_state_singleton->run_multiplier              = 1.5;
+		__act_player_state_singleton->walk_multiplier             = 1.0;
+		
+		__act_player_state_singleton->__distance_to_ground        = 0.0;
+		__act_player_state_singleton->__quants_covered            = 0.0;
+		
+		__act_player_state_singleton->can_stand                   = true;
+		__act_player_state_singleton->can_climb                   = false;
+		__act_player_state_singleton->is_moving                   = false;
+		__act_player_state_singleton->can_move                    = true;
+		
+		double d = dtimer();
+		game_log_write( str_printf(NULL, "Finished building a home for the soul. (%f seconds)", d/1000) );
+	}
+}
+
+__static void __act_player_update_camera()
+{
+	if(__act_player_state_singleton->can_move && __act_player_state_singleton->__move_type != MOVE_ON_LADDER)
+	{
+		__act_player_state_singleton->__cam_ang.pan = cycle(__act_player_state_singleton->__cam_ang.pan - mickey.x / 6.5 * 1.0, 0, 360);
+		__act_player_state_singleton->__cam_ang.tilt = clamp(__act_player_state_singleton->__cam_ang.tilt - mickey.y / 6.5 * 1.0, -90, 90);
+		__act_player_state_singleton->__cam_ang.roll = 0;
+	}
+	
+	vec_set( &camera->pan, &(__act_player_state_singleton->__cam_ang) );
+	vec_set( &(__act_player_state_singleton->__cam_pos), vector(0, 0, __act_player_state_singleton->cam_height));
+	vec_rotate( &(__act_player_state_singleton->__cam_pos) , vector(camera->pan, 0, 0));
+	vec_add( &(__act_player_state_singleton->__cam_pos), &my->x );
+	vec_set( &camera->x, vector(__act_player_state_singleton->__cam_pos.x, __act_player_state_singleton->__cam_pos.y, camera->z) );
+	__act_player_state_singleton->cam_lerp = __act_player_state_singleton->cam_smooth * time_step;
+	
+	camera->z -= (camera->z - __act_player_state_singleton->__cam_pos.z) * __act_player_state_singleton->cam_lerp;
+	camera->z = clamp(camera->z, __act_player_state_singleton->__cam_pos.z - __act_player_state_singleton->cam_smooth_offset, __act_player_state_singleton->__cam_pos.z + __act_player_state_singleton->cam_smooth_offset);
+}
+
+__static void __act_player_scan_foot()
+{
+	float old = my->min_z;
+	long mode = IGNORE_ME | IGNORE_PASSABLE | IGNORE_PASSENTS | IGNORE_MAPS | IGNORE_SPRITES | IGNORE_CONTENT | IGNORE_FLAG2 | USE_BOX;
+	
+	c_ignore(PUSH_GROUP, 0);
+	my->min_z = -0.125;
+	c_trace( &my->x, vector(my->x, my->y, my->z - 256.0), mode );
+	my->min_z = old;
+	
+	// get lowest position of the bbox:
+	vec_for_min( &(__act_player_state_singleton->__bbox_lowest_pos), my);
+	// reset distance:
+	__act_player_state_singleton->__distance_to_ground = 999;
+	
+	if(trace_hit)
+	    __act_player_state_singleton->__distance_to_ground  = my->z + __act_player_state_singleton->__bbox_lowest_pos.z - target.z; // find height to the ground:
+	    
+	vec_set( &(__act_player_state_singleton->__platform_move_speed), nullvector);
+	
+	if(you != NULL) // if we are standing on something
+	{
+		if( your->OBJECT_TYPE == OBJECT_PLATFORM ) // platform?
+		    vec_set( &(__act_player_state_singleton->__platform_move_speed), vector(your->SPEED_X, your->SPEED_Y, 0) ); // save your movement speed:
+		    
+		// more cases here...
+	}
+}
+
+/*
+ * __act_player_register_physics()
+ * 
+ * Register the player to the physics engine.
+ */
+__static void __act_player_register_physics()
+{
+	game_log_write("Now registering the soul to the physics engine.");
+	
+	#ifdef ACKPHYSX_H		
+	if(__GameState_singleton->__game_physx_loaded__)
+	{
+		pXent_settype(my, PH_RIGID, PH_CAPSULE);
+		pXent_setmass(my, 5);
+		pXent_setskinwidth(my, 0.01);
+		pXent_setbodyflag(my, NX_BF_FROZEN_ROT, 1);
+		pXent_setdamping(my, 50, 100);
+		pXent_setmaterial(my, vector(1, 0, 0), vector(0, 0, NX_MF_DISABLE_FRICTION | NX_MF_DISABLE_STRONG_FRICTION), NULL);
+		
+		game_log_write("Done registering.");
+	}
+	else
+	{
+		game_log_write("Failed to register the soul. (PhysX failed to initialize)");
+	}
+	#else
+	    game_log_write("Header <ackphysX> wasn't included in the current build; cannot register.");
+	#endif
+}
+
+__static void __act_player_create_bbox()
+{
+	__act_player_state_singleton->__object_stand = ent_create( game_asset_get_object("bbox_stand.mdl") , &my->x, NULL); // create stand shape
+	__act_player_state_singleton->__object_stand->flags |= (INVISIBLE | PASSABLE);
+	
+	while(proc_status(ent_create)) wait(1.0);
+	c_setminmax(__act_player_state_singleton->__object_stand);
+}
+
+/*
+ * void act_player()
+ * 
+ * Player action. Should be assigned through WED.
+ */
 action act_player()
 {
 	#ifdef ACKPHYSX_H
