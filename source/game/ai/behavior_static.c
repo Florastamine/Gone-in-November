26113f3c5ac809badf5628f451d68f6ad45e92ca
Florastamine <remilla.scarlet.94@gmail.com>
2015-12-16 09:39:31 +0700

    Generic rigid body

--- source/game/ai/behavior_static.c
+++ source/game/ai/behavior_static.c
@@ -2,23 +2,65 @@
 /*
  *             DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
  *                     Version 2, December 2004
- * 
+ *
  *  Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
- * 
+ *
  *  Everyone is permitted to copy and distribute verbatim or modified
  *  copies of this license document, and changing it is allowed as long
  *  as the name is changed.
  *
  *             DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
  *    TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
- * 
+ *
  *   0. You just DO WHAT THE FUCK YOU WANT TO.
  * __________________________________________________________________
- * 
+ *
  * <behavior_static>
- * 
+ *
  * Contains code for general, static entities.
- * 
+ *
  * Authors: Huy Nguyen (http://vn-sharing.net/forum/member.php?u=15466)
  * __________________________________________________________________
  */
+
+/*
+ * action act_rigid_body()
+ *
+ * Simple yet generic rigid body.
+ */
+action act_rigid_body()
+{
+    my->flags |= (POLYGON);
+
+ 	c_setminmax(my);
+ 	my->push = OBSTACLE_GROUP;
+
+    wait(1);
+    switch((int) my->skill1)
+    {
+        case    BODY_CONVEX:
+        {
+            pXent_settype(my, PH_RIGID, PH_CONVEX);
+            break;
+        }
+
+        case    BODY_BOX:
+        {
+            pXent_settype(my, PH_RIGID, PH_BOX);
+            break;
+        }
+
+        case    BODY_SPHERE:
+        {
+            pXent_settype(my, PH_RIGID, PH_SPHERE);
+            break;
+        }
+
+        default:
+        {
+            game_log_write(str_printf(my->string1, "(Warning!) Entity %s contains invalid body types for registering as physics entity.", _chr(str_for_entname(NULL, my)) ));
+        }
+    }
+
+ 	pXent_setgroup(my, OBSTACLE_GROUP);
+}
--- source/game/ai/behavior_static.h
+++ source/game/ai/behavior_static.h
@@ -2,28 +2,43 @@
 /*
  *             DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
  *                     Version 2, December 2004
- * 
+ *
  *  Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
- * 
+ *
  *  Everyone is permitted to copy and distribute verbatim or modified
  *  copies of this license document, and changing it is allowed as long
  *  as the name is changed.
  *
  *             DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
  *    TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
- * 
+ *
  *   0. You just DO WHAT THE FUCK YOU WANT TO.
  * __________________________________________________________________
- * 
+ *
  * <behavior_static>
- * 
+ *
  * Contains code for general, static entities.
- * 
+ *
  * Authors: Huy Nguyen (http://vn-sharing.net/forum/member.php?u=15466)
  * __________________________________________________________________
  */
 #ifndef    __BEHAVIOUR_STATIC_H__
 #define    __BEHAVIOUR_STATIC_H__
+
+/*
+ * Body types for use within act_rigid_body().
+ */
+#define    BODY_CONVEX    1
+#define    BODY_BOX       2
+#define    BODY_SPHERE    3
+
+/*
+ * action act_rigid_body()
+ *
+ * Simple yet generic rigid body.
+ * Entity's skill1 is reserved for body types (see above).
+ */
+action act_rigid_body();
 
 #include "behavior_static.c"
 #endif /* behavior_static.h */
